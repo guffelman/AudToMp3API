414a3cac9fe44bfcc9692ad036b84567b1bcfdd7
guffelman <garrett@garrettkeith.com>
2023-11-07 13:56:17 -0500

    stopped storing files to fs

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import multer from 'multer';
-import fs from 'fs';
-import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 import ffmpeg from 'fluent-ffmpeg';
-
+import { Readable } from 'stream';
+import path from 'path';
 const app = express();
 
 const storage = multer.memoryStorage();
@@ -15,9 +14,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// route for index
 app.get('/', (req, res) => { 
-  // serve text
   res.send('Wav to Mp3 Converter');
 });
 
@@ -34,59 +31,20 @@ app.post('/convert', upload.single('file'), (req, res) => {
     return res.status(400).send('Invalid file type');
   }
 
-  const filename = `${uuidv4()}.wav`;
-  const filePath = path.join(path.dirname(new URL(import.meta.url).pathname), 'uploads', filename);
+  const readStream = new Readable();
+  readStream._read = () => {};
+  readStream.push(file.buffer);
+  readStream.push(null);
 
-  // Use fs.writeFile to write the file asynchronously
-  fs.writeFile(filePath, file.buffer, (err) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Internal Server Error');
-    }
+  const mp3Filename = `${uuidv4()}.mp3`;
 
-    convertWavToMp3(filePath)
-      .then((mp3Filename) => {
-        res.download(mp3Filename, (err) => {
-          if (err) {
-            console.error(err);
-          }
-          // Use fs.unlink to delete files asynchronously
-          fs.unlink(filePath, (err) => {
-            if (err) {
-              console.error(err);
-            }
-          });
-          fs.unlink(mp3Filename, (err) => {
-            if (err) {
-              console.error(err);
-            }
-          });
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
-      });
-  });
+  res.setHeader('Content-Disposition', `attachment; filename=${mp3Filename}`);
+
+  ffmpeg(readStream)
+    .audioCodec('libmp3lame')
+    .toFormat('mp3')
+    .pipe(res);
 });
-
-function convertWavToMp3(wavFilename) {
-  return new Promise((resolve, reject) => {
-    const outputFile = wavFilename.replace('.wav', '.mp3');
-
-    ffmpeg()
-      .input(wavFilename)
-      .audioCodec('libmp3lame')
-      .toFormat('mp3')
-      .on('end', () => {
-        resolve(outputFile);
-      })
-      .on('error', (err) => {
-        reject(err);
-      })
-      .save(outputFile);
-  });
-}
 
 app.listen(443, () => {
   console.log('Server listening on port 443');
